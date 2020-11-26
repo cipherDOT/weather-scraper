d45fe765dbe917e75ef6ac510d0f1325d9b73d15
Kavin Bharathi <66783110+cipherDOT@users.noreply.github.com>
2020-11-26 11:23:58 +0530

    Add files via upload

---
+++ weather scraper.py
@@ -0,0 +1,40 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+
+import time
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+chromedriver = os.getenv('chromedriver')
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+browser = webdriver.Chrome(
+    chromedriver,
+    chrome_options=chrome_options
+)
+
+# place is an user input
+place = input('Please enter the place of interest: ')
+
+# url for getting the tim information from Google
+url = f'https://www.google.com/search?q=weather at {place}'
+
+# browser to get the url(without opening a browser due to '--headless' argument)
+browser.get(url)
+
+# giving time for the browser to load the page
+time.sleep(2)
+
+# Handling the NoSuchElement error
+try:
+    temp = browser.find_element_by_xpath('//*[@id="wob_tm"]')
+except NoSuchElementException as e:
+    print(e)
+
+# Time output
+print(f'The temperature in {place.capitalize()} now is {temp.text}°C')
+
